f616e9e446aebda4f67bc7229e8b53a74dc816c6
debdevs <deborahmdaniels1@gmail.com>
2022-09-01 18:24:55 -0400

    added paragraph styling and button for header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,6 +8,8 @@ const Header = () => (
     <div className = "app__wrapper_info">
     <SubHeading title ="Chase the new flavor"/>
    <h1 className ="app__header-h1">The Key To Fine Dining</h1>  
+   <p className = "p__opensans" style = {{margin: '2rem 0'}}>This is a paragraph to describe wonderful, elegant & delicious food</p>
+   <button type = "button" className = "custom__button">Explore Menu</button>
 
     </div>
   <div className = "app__wrapper_img">
