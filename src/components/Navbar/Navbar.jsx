555c15445c62c50fc05eab3b831aa1a5bea94526
debdevs <deborahmdaniels1@gmail.com>
2022-09-01 16:23:27 -0400

    improved mobile responsiveness and linked css variables
    added media queriesand linked to global variables for navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -29,16 +29,20 @@ const Navbar = () => {
         </div>
       <div className='app__navbar-smallscreen'>
         <GiHamburgerMenu color = "fff" fontSize={27} onClick ={() => setToggleMenu(true)} />
-        <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
-          <MdOutlineRestaurantMenu fontSize={27} className ='overlay__close' onClick ={() => {setToggleMenu(false)}} />
-      
-        <ul className = "app__navbar-smallscreen-links">
-          <li className = "p__opensans"> <a href = "#home"></a>Home</li>
-          <li className = "p__opensans"> <a href = "#about"></a>About</li>
-          <li className = "p__opensans"> <a href = "#menu"></a>Menu</li>
-          <li className = "p__opensans"> <a href = "#menu"></a>Contact</li>
-        </ul>
-        </div>
+
+        {toggleMenu && (
+          <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+            <MdOutlineRestaurantMenu fontSize={27} className ='overlay__close' onClick ={() => {setToggleMenu(false)}} />
+        
+            <ul className = "app__navbar-smallscreen_links">
+              <li className = "p__opensans"> <a href = "#home"></a>Home</li>
+              <li className = "p__opensans"> <a href = "#about"></a>About</li>
+              <li className = "p__opensans"> <a href = "#menu"></a>Menu</li>
+              <li className = "p__opensans"> <a href = "#menu"></a>Contact</li>
+            </ul>
+          </div>
+
+        )}
 
 
       </div>
