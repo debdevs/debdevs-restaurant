ddee3a1ae139550da4b92669e9a8dd8649169e02
debdevs <deborahmdaniels1@gmail.com>
2022-09-01 11:35:51 -0400

    First commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import{GIHamburgerMenu} from 'react-icons/gi' ;
+import{MdOutlineRestaurantMenu} from 'react-icons/md' ;
+import './Navbar.css';
+
+const Navbar = () => (
+  <div>
+    Navbar
+  </div>
+);
+
+export default Navbar;
