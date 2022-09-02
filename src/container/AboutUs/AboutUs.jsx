372c9a79e0223ec3113201e611459fb00d60dc92
debdevs <deborahmdaniels1@gmail.com>
2022-09-01 21:58:28 -0400

    added hover effect to header image

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import {images} from '../../constants';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app__aboutus ap__bg flex__center section__padding' id = 'about'>
+  <div className='app__aboutus-overlay flex__center '>
+    <img src={images.G} alt = "g ;etter"/>
+  </div>
   </div>
 );
 
