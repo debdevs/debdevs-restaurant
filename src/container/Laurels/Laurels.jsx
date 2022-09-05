f35bb196c08c1d2441012361bb879ef4f81e31ed
debdevs <deborahmdaniels1@gmail.com>
2022-09-05 00:11:33 -0400

    fixed horizontal mobile scrolling issue by addin " auto" to image width
    image in the special menu section was breaking scrolling for mobile. added width to media query and placed image div in proper location.

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -22,12 +22,14 @@ const Laurels = () => (
       <SubHeading title = "Awards & Recognition"/>
       <h1 className = "headtext__cormorant">Our Laurels</h1>
       <div className = "app__laurels_awards">
-
         {data.awards.map((award) => <AwardCard award = {award} key = {award.title}/> )}
       </div>
-  
+
     </div>
-    
+    <div className='app__wrapper_img'>
+      <img src = {images.laurels} alt = "laurels"/>
+    </div>
+
   </div>
 );
 
